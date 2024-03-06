9a7b56136ff7b112e233fed592fab90956b7466c
tengtaox <tetaciao@gmail.com>
2024-03-06 13:00:29 +0800

    2024.3.6

--- Leetcode/27. 移除元素.cpp
+++ Leetcode/27. 移除元素.cpp
@@ -8,10 +8,10 @@ public:
         int n = nums.size();
         int i = 0, j = n - 1;
         int count = 0;
-        for (;i <= j;) {
+        for (;i < j;) {
             for(; i < n && nums[i] != val;i++);
             for(; j >= 0  && nums[j] == val;j--);
-            if (i <= j){
+            if (i < j){
                 swap(nums[i],nums[j]);
                 count++;
             }
@@ -19,12 +19,3 @@ public:
         return i;
     }
 };
-
-int main() {
-    Solution solution;
-    vector<int> nums = {2};
-    int val = 3;
-    int len = solution.removeElement(nums, val);
-    cout << len;
-    return 0;
-}
