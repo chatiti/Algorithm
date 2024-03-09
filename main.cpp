a3caf32e3473b3ae19442b2dff1a9f4dce8c3c12
tengtaox <tetaciao@gmail.com>
2024-03-10 00:07:36 +0800

    169

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int removeElement(vector<int> &nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         int n = nums.size();
         int i = 0, j = n - 1;
         int count = 0;
-        for (;i <= j;) {
-            for(; i < n && nums[i] != val;i++);
-            for(; j >= 0  && nums[j] == val;j--);
-            if (i <= j){
-                swap(nums[i],nums[j]);
+        for (; i <= j;)
+        {
+            for (; i < n && nums[i] != val; i++)
+                ;
+            for (; j >= 0 && nums[j] == val; j--)
+                ;
+            if (i <= j)
+            {
+                swap(nums[i], nums[j]);
                 count++;
             }
         }
@@ -20,9 +26,11 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
-    vector<int> nums = {2};
+    vector<int> nums;
+    nums.push_back(2);
     int val = 3;
     int len = solution.removeElement(nums, val);
     cout << len;
